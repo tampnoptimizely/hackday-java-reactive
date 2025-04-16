856105f2b565e2180622ce2b2304c7a8febb52af
tam pham <tam.pham@optimizely.com>
2025-04-16 10:24:08 +0700

    Refactoring code

--- src/main/java/com/optimizely/javareactive/repositories/StockPriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.optimizely.javareactive.repositories;
-
-import com.optimizely.javareactive.documents.StockPriceEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface StockPriceRepository extends ReactiveCrudRepository<StockPriceEntity, Long> {
-}
